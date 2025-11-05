b6f379d2f4dbc99ffbc54dbef301d8d49ceb4619
Rob Evans <rob@irrelon.com>
2025-11-05 20:06:38 +0000

    Initial Commit

---
+++ Port Inspector/Port_InspectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Port_InspectorApp.swift
+//  Port Inspector
+//
+//  Created by Rob Evans on 05/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Port_InspectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Port InspectorTests/Port_InspectorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Port_InspectorTests.swift
+//  Port InspectorTests
+//
+//  Created by Rob Evans on 05/11/2025.
+//
+
+import Testing
+@testable import Port_Inspector
+
+struct Port_InspectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
